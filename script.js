952bc75685d46155030bc94f28681693fabf66be
G <gpanchal0624@gmail.com>
2026-01-21 14:03:37 +0530

    Day 9: refactor logic using functions and multiple actions

--- script.js
+++ script.js
@@ -3,7 +3,25 @@ let count = 0;
 const countDisplay = document.getElementById("count");
 const increaseBtn = document.getElementById("increaseBtn");
 
-increaseBtn.addEventListener("click", function (){
-    count++;
+function updateCount(newValue){
+    count = newValue;
     countDisplay.textContent = count;
+}
+
+increaseBtn.addEventListener("click", function(){
+    updateCount(count + 1);
+    /*count = count + 1;*/
+    /*countDisplay.textContent = count;*/
+});
+
+resetBtn.addEventListener("click", function(){
+    updateCount(0);
+    /*count = 0;*/
+    /*countDisplay.textContent = count;*/
+});
+
+decreaseBtn.addEventListener("click", function(){
+    updateCount(count - 1);
+    /*count = count - 1;*/
+    /*countDisplay.textContent = count;*/
 });
